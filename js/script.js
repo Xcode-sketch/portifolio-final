f55fba2a9af8e9b345a172891a0794871a7bdec5
Eduardo Barbosa Silva <eduardobarbosasilva.oficial@outlook.com>
2025-09-06 01:46:35 -0300

    feat: adicionado incio de um dark mode

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
+const theme = document.body.querySelector("#theme");
+const body = document.body;
 
+const root = document.documentElement;
+
+
+function darkTheme(isdark) {
+  if(isdark == true) {
+    body.classList.add('dark');
+    theme.innerHTML = '<a href="#"><i class="fa-solid fa-sun"></i></i></a>';
+    root.style.setProperty('--destaque', '#000000ff');
+  } else {
+    body.classList.remove('dark');
+    theme.innerHTML = '<a href="#"><i class="fa-solid fa-moon"></i></i></a>';
+    root.style.setProperty('--destaque', '#0077ff');
+  }
+}
+
+theme.addEventListener('click', () => {
+  const isdark = body.classList.toggle('dark');
+  darkTheme(isdark);
+  localStorage.setItem('theme', 'isdark' ? 'dark' : 'white');
+})
 
 // Scroll suave para links de navegação
 const navLinks = document.querySelectorAll('#menu ul a.link');
